b58218df4e00de0ab81c360cae1b65c126760b09
Korvin1337 <korvin1337@gmail.com>
2025-05-20 17:34:55 +0200

    initpush

---
+++ Persistance/Contexts/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistance.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Contexts;
+
+public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+{
+    public DbSet<EventEntity> Events { get; set; }
+
+    public DbSet<PackageEntity> Packages { get; set; }
+
+    public DbSet<EventPackageEntity> EventPackages { get; set; }
+}
---
+++ Persistance/Models/RepositoryResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistance.Models;
+
+public class RepositoryResult
+{
+    public bool Success { get; set; }
+    public string? Error { get; set; }
+}
+
+public class RepositoryResult<T> : RepositoryResult
+{
+    public T? Result { get; set; }
+}
